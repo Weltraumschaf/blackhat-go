90f98717706df683eb58c0e896dbd8ac5e226fb4
Sven Strittmatter <ich@weltraumschaf.de>
2021-12-09 21:38:08 +0100

    Implement very simple proxy
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/proxy/app.go
+++ internal/proxy/app.go
@@ -4,6 +4,7 @@ import (
 	"github.com/urfave/cli/v2"
 	"io"
 	"log"
+	"weltraumschaf.de/blackhat/internal/lib"
 	"net"
 )
 
@@ -22,30 +23,39 @@ func Create() *cli.App {
 }
 
 func Execute(c *cli.Context) error {
-	listener, err := net.Listen("tcp", ":20080")
+	listener, err := net.Listen(lib.Tcp.String(), lib.CreateAddress("", 80))
 	if err != nil {
-		log.Fatalln("Unable to bind port")
+		log.Fatalln("Unable to bind to port 80")
 	}
-
-	log.Println("Listening on 0.0.0.0:20080")
 
 	for {
 		conn, err := listener.Accept()
-		log.Println("Received connection")
-
 		if err != nil {
 			log.Fatalln("Unable to accept connection")
 		}
 
-		go echo(conn)
+		go handle(conn)
 	}
 
 	return nil
 }
 
-func echo(conn net.Conn) {
-	defer conn.Close()
-	if _, err := io.Copy(conn, conn); err != nil {
-		log.Fatalln("Unable to read/write data")
+func handle(src net.Conn) {
+	address := lib.CreateAddress("neverssl.com", 80)
+	dst, err := net.Dial(lib.Tcp.String(), address)
+	if err != nil {
+		log.Fatalln("Unable to connect")
+	}
+
+	defer dst.Close()
+
+	go func() {
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if _,err := io.Copy(src, dst); err != nil {
+		log.Fatalln(err)
 	}
 }
